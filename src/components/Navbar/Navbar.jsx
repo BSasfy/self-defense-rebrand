44319c2db7ca6212b7819d588d0de29730b0fb0a
BSasfy <barbara.sasfy@gmail.com>
2024-07-27 00:36:36 +0100

    add Navbar to Layout

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import './Navbar.css';
+
+export default function Navbar() {
+    return (
+        <ul>
+            <li className='active'><a href="#">Home</a></li>
+            <li><a href="#">Classes</a></li>
+            <li><a href="#">Trial</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul> 
+    )
+}
+
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import '@radix-ui/themes/styles.css';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Theme>
+      <App />
+      {/* <ThemePanel /> */}
+    </Theme>
   </React.StrictMode>,
 )
