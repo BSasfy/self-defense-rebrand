2aea6d6e526640f90279c6920ab690943766d984
BSasfy <barbara.sasfy@gmail.com>
2024-08-01 22:36:34 +0100

    add background color and cards to homepage

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -1,9 +1,11 @@
+import { Link } from "react-router-dom";
+import CardItem from "../components/Card/CardItem";
 import styles from "./home.module.css";
 
 export default function Home() {
   return (
     <>
-      <div className={styles.container}>
+      <div className={styles.headerContainer}>
         <img
           src="/images/SSDSSelfDefeceFrontPageWhite.jpg"
           alt="Self Defence Group Picture"
@@ -15,6 +17,16 @@ export default function Home() {
             <span style={{ display: "block" }}>SCOTLAND</span>
           </h1>
           <h2>Adult classes in Glasgow - at Glasgow Club Kelvinhall</h2>
+        </div>
+      </div>
+      <div className={styles.pageContainer}>
+        <div className={styles.cardsContainer}>
+          <Link to="/classes/1">
+            <CardItem />
+          </Link>
+
+          <CardItem />
+          <CardItem />
         </div>
       </div>
     </>
